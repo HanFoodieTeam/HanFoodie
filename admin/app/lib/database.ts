511c19492729f0cc554f2e8d3f639210614dd7ce
Đức <122283678+lauvanduc@users.noreply.github.com>
2025-10-16 14:57:17 +0700

    CRUD api - mã giảm giá

--- admin/app/lib/database.ts
+++ admin/app/lib/database.ts
@@ -1,14 +1,3 @@
-// lib/database.ts
-// import { Sequelize } from "sequelize";
-// export const db = new Sequelize({
-//   dialect: "mysql",
-//   host: process.env.DB_HOST,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   logging: false, // Tắt log SQL
-//   dialectModule: require('mysql2'),
-// });
 
 import mysql2 from "mysql2";
 import { Sequelize } from "sequelize";
