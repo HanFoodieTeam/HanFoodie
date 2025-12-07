4e7f4b2b26c9be2b34b0d98a3b26d73b069c659f
Noc Noc <ngocnbps38216@gmail.com>
2025-12-08 02:41:52 +0700

    không any , chỉnh ảnh hồ sơ,footer

--- user/app/layout.tsx
+++ user/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { CartProvider } from "./context/giohangcontext";
 
 export const metadata: Metadata = {
@@ -23,6 +24,7 @@ export default function RootLayout({
             <div className="max-w-[80%] mx-auto px-4">{children}</div>
           </main>
         </CartProvider>
+        <Footer />
       </body>
     </html>
   );
--- user/app/page.tsx
+++ user/app/page.tsx
@@ -140,7 +140,6 @@ export default function TrangChuPage() {
         </div>
       </main>
 
-      <Footer />
     </>
     
   );
